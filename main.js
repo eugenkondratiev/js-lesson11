d9a8022cb5706b6eb74bb3238bfcc47026c194cb
eugenkondratiev <kondratiev.es@gmail.com>
2019-03-25 07:41:06 +0200

    Server created

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("First start");
+
+//=======================================
+
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+//const lcl = require('/home//eugenkondratiev//le')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/plain");
+    res.end(`Server running at http://${hostname}:${port}`);
+
+});
+
+server.listen(port, hostname, () => {
+    console.log("Server created");
+    
+});
+
+ 
+
+console.log('Finish');
