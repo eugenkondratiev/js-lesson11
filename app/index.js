ffbc528321f051cc9fb4ca8e55ea21d66c38ba3a
eugenkondratiev <kondratiev.es@gmail.com>
2019-03-27 19:19:15 +0200

    Initial code. Scripts added.

---
+++ app/index.js
@@ -0,0 +1,7 @@
+
+const model = new Students();
+const view = new StudentsView('#main-container');
+const controller = new StudentsController(model, view);
+
+controller.setViewContext('#main-container');
+controller.showStudents();
